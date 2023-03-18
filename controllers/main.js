6b1a54be05b8dd55c0f2bd1d9275044a61760473
Saul Chipwayambokoma <chipwayambokomas@gmail.com>
2023-03-18 08:34:34 +0200

    Created my first jsonWebToken

--- controllers/main.js
+++ controllers/main.js
@@ -1,11 +1,25 @@
+const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
 
 const login = async (req,res) =>{
-    res.send('Fake login/regidter/signup')
+  
+    const {username,password} = req.body  
+
+    if(!username || !password){
+        throw new CustomAPIError('Please provide email and a password.', 404)
+    }
+
+    const id = new Date().getDate()
+
+    const token = jwt.sign({id, username},process.env.jsonWebToken_Secret,{expiresIn: '30d'})
+
+    console.log(username, password);
+    res.status(200).json({msg: `User token created, ${token}`})
 }
 
 const dashboard = async (req,res) => {
     const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg:`Hello, Saul`, secret: `Your luck number is: ${luckyNumber}`})
+    res.status(200).json({msg:`Hello, Saul`, secret: `Your lucky number is: ${luckyNumber}`})
 }
 
 module.exports = {
